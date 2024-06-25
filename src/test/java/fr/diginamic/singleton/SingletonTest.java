c1b1d0e42f58dcb57a2a3418c7fd50e9239fb67d
AyoubBenziza <ayoub.cbenziza@gmail.com>
2024-06-25 15:12:11 +0200

    update: Exercise 4 SingletonTest.java

--- src/test/java/fr/diginamic/singleton/SingletonTest.java
+++ src/test/java/fr/diginamic/singleton/SingletonTest.java
@@ -2,16 +2,29 @@ package fr.diginamic.singleton;
 
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 public class SingletonTest {
 
     @Test
+    public void testGetInstance() {
+        Configuration instance = Configuration.getInstance();
+        assertNotNull(instance);
+    }
+    @Test
     public void testGetProperty() {
         Configuration instance = Configuration.getInstance();
-        assert(instance.getProperty("theme").equals("dark"));
-        assert(instance.getProperty("title").equals("Design Patterns"));
-        assert(instance.getProperty("language").equals("fr"));
-        assert(instance.getProperty("db.user").equals("root"));
-        assert(instance.getProperty("db.password").equals("pw"));
+        assertEquals("dark", instance.getProperty("theme"));
+        assertEquals("Design Patterns", instance.getProperty("title"));
+        assertEquals("fr", instance.getProperty("language"));
+        assertEquals("root", instance.getProperty("db.user"));
+        assertEquals("pw", instance.getProperty("db.password"));
+    }
+
+    @Test
+    public void testGetPropertyWithNull() {
+        Configuration instance = Configuration.getInstance();
+        assertNull(instance.getProperty("unknown"));
     }
 
     @Test
